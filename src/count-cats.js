9e683b0980673016cb2f83c62daca3c930775089
kamelot88 <Vasilchenkoov1988@gmail.com>
2023-04-17 21:40:14 +0200

    feat: add solution of an algorithmic problem countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,10 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// let matriX = [[0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ];
+function countCats(backyard) {
+  // throw new NotImplementedError('Not implemented');
+  let count = 0;
+  for(let elem of backyard) {
+    for (let i = 0; i < elem.length; i++) {
+      if(elem[i] === '^^') {
+        count++;
+      }
+    }
+  }
+  return count;
 }
+
 
 module.exports = {
   countCats
